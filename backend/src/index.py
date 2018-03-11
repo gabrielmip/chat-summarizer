05e3873316c60cdaa35db23987c80e4e0fc919ed
Gabriel Pedrosa <gabriel.pedrosa@waycarbon.com>
2018-03-10 22:57:08 -0300

    Rota no flask em / que retorna mensagens sumarizadas e os tópicos encontrados

--- backend/src/index.py
+++ backend/src/index.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify, Response
+from summa import keywords as keywordExtractor
+from summa import summarizer
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def helloWorld ():
-  return 'Hello, World!'
+@app.route('/', methods=['POST'])
+def summary ():
+  messages = json.loads(request.data)['messages']
+  concatenatedMessages = '. '.join(messages)
+  summarized = summarizer.summarize(concatenatedMessages)
+  keywords = keywordExtractor.keywords(concatenatedMessages)
+  responseContent = {
+    'summarized': summarized,
+    'keywords': keywords
+  }
+  response = jsonify(responseContent)
+  response.status_code = 200
+  return response
